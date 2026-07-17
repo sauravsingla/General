af25b6a6af8b87d5e70e766c6e6652aed3ac68d0
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-07-17 13:11:31 +0530

    Expose comparison and graph APIs

--- src/general_reference/__init__.py
+++ src/general_reference/__init__.py
@@ -1,12 +1,21 @@
 """Reusable reference implementations for common technical problems."""
 
-from .algorithms import binary_search, longest_increasing_subsequence, merge_sort
+from .algorithms import (
+    binary_search,
+    longest_increasing_subsequence,
+    longest_increasing_subsequence_dp,
+    merge_sort,
+)
+from .graphs import breadth_first_path, dijkstra_shortest_path
 from .statistics import RunningStatistics, online_mean_variance
 
 __all__ = [
     "RunningStatistics",
     "binary_search",
+    "breadth_first_path",
+    "dijkstra_shortest_path",
     "longest_increasing_subsequence",
+    "longest_increasing_subsequence_dp",
     "merge_sort",
     "online_mean_variance",
 ]
